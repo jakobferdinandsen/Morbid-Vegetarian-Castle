c17e4f33113e7cd79673c371249f9f8b1258d90c
Jakob Ferdinandsen <jakob@jhaaf.dk>
2017-03-17 13:52:34 +0100

    Added some placeholder assets & a playerscript

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D playerObject;
+
+    // Use this for initialization
+    void Start() {
+        playerObject = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+
+        }
+        if (Input.GetKey(KeyCode.UpArrow)) {
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) { }
+}

8a93bffe8ebe19c400ad1d0969de313955867668
Jakob Ferdinandsen <jakob@jhaaf.dk>
2017-04-21 09:56:10 +0200

    Added level1

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -5,6 +5,7 @@ using UnityEngine.WSA;
 
 public class Follower : MonoBehaviour
 {
+    public Transform target;
     public float speed;
 
     private Rigidbody2D playerObject;
@@ -12,30 +13,26 @@ public class Follower : MonoBehaviour
     private GameObject mapBoundary;
 
     // Use this for initialization
-    void Start()
-    {
+    void Start() {
         playerObject = GetComponent<Rigidbody2D>();
         nonTraversables = GameObject.FindGameObjectsWithTag("nontraversable");
         mapBoundary = GameObject.FindGameObjectWithTag("mapboundary");
 
-        foreach (GameObject nonTraversable in nonTraversables)
-        {
-            Transform pos = nonTraversable.GetComponent<Transform>();
-            Boundary boundary = new Boundary(pos);
-            Debug.Log(boundary.left);
-
-        }
+//        foreach (GameObject nonTraversable in nonTraversables)
+//        {
+//            Transform pos = nonTraversable.GetComponent<Transform>();
+//            Boundary boundary = new Boundary(pos);
+//            Debug.Log(boundary.left);
+//
+//        }
     }
 
-    void Update()
-    {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-        transform.position += move * speed * Time.deltaTime;
+    void Update() {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
     }
 
-    void OnCollisionEnter2D(Collision2D coll)
-    {
-    }
+    void OnCollisionEnter2D(Collision2D coll) { }
 
     public class Boundary
     {
@@ -44,20 +41,16 @@ public class Follower : MonoBehaviour
         public float top { get; set; }
         public float bottom { get; set; }
 
-        public Boundary(Transform transform)
-        {
-            left = transform.position.x-(transform.lossyScale.x/2);
-            right = transform.position.x+(transform.lossyScale.x/2);
-            top = transform.position.y+(transform.lossyScale.y/2);
-            bottom = transform.position.y-(transform.lossyScale.y/2);
+        public Boundary(Transform transform) {
+            left = transform.position.x - (transform.lossyScale.x / 2);
+            right = transform.position.x + (transform.lossyScale.x / 2);
+            top = transform.position.y + (transform.lossyScale.y / 2);
+            bottom = transform.position.y - (transform.lossyScale.y / 2);
         }
 
 
-        public override string ToString()
-        {
-            return "left: " + left+", right: "+right;
+        public override string ToString() {
+            return "left: " + left + ", right: " + right;
         }
     }
-
-
 }

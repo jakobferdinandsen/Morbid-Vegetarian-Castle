ea8019c5889ca12b498cc42360c3657d3bdecb20
Jakob Ferdinandsen <jakob@jhaaf.dk>
2017-04-27 13:39:14 +0200

    Added heatlh script

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour{
+    public int health = 3;
+
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Enemy") || other.gameObject.CompareTag("EnemyProjectile")) {
+            health--;
+            GameObject[] healthObjects = GameObject.FindGameObjectsWithTag("Health");
+            Destroy(healthObjects[healthObjects.Length - 1]);
+            Destroy(other.gameObject);
+            if (health == 0) {
+                //TODO Show game over screen
+            }
+        }
+    }
+}

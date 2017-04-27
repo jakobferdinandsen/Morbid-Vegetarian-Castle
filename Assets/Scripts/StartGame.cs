9b31a8a4b8235931c1bccdba00aeafc968ed839b
Diphta <dino-355@hotmail.com>
2017-04-27 23:01:16 +0200

    Menu
    Jeg er stupid til det her xD

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+
+    public void gameStart()
+    {
+        Application.LoadLevel("Level_1");
+    }
+}

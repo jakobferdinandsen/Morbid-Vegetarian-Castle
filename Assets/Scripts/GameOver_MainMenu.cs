360e37780622b79c2f9122fd33e994851f6bb396
Diphta <dino-355@hotmail.com>
2017-04-28 00:43:35 +0200

    Main menu og game over skærm done

---
+++ Assets/Scripts/GameOver_MainMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver_MainMenu : MonoBehaviour {
+
+    public void loadMenu()
+    {
+        Application.LoadLevel("Main_Menu");
+    }
+}
